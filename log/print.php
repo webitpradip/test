703fdbbc8371f3e7bcebca73e5895351a30f0c4f
Pradip Mondal <web.it.pradip@gmail.com>
2024-06-30 00:02:43 +0530

    Logging enabled for classes and methods

--- log/print.php
+++ log/print.php
@@ -1,6 +1,107 @@
 <?php require_once 'menu.php';?>
 <?php
+function addErrorLogging($filePath) {
+    // Regular expression to match any function definition and capture the parameters
+    $code = $content = file_get_contents($filePath);
+    $pattern = '/(function\s+\w+\s*\(([^)]*)\)\s*\{)/';
 
+    // Callback function to append error_log after the opening brace
+    $callback = function ($matches) {
+        $functionHeader = $matches[1];
+        $params = $matches[2];
+
+        $errorLog = "    error_log(__FILE__.':'.__LINE__);";
+
+        // Add error logging for each parameter
+        if (!empty(trim($params))) {
+            $paramList = explode(',', $params);
+            foreach ($paramList as $param) {
+                $param = trim($param);
+                $paramName = $param;
+                $paramName = str_replace('$','',$paramName);
+                $errorLog .= "\n    error_log('$paramName: ' . print_r($param, true));";
+            }
+        }
+
+        return $functionHeader . "\n" . $errorLog;
+    };
+
+    // Perform the replacement
+    $modifiedCode = preg_replace_callback($pattern, $callback, $code);
+
+    file_put_contents($filePath, $modifiedCode); 
+}
+function add_logging_to_methods($filePath) {
+    $content = file_get_contents($filePath);
+    $lines = explode("\n", $content);
+
+    $method_pattern = '/function\s+(\w+)\s*\(([^)]*)\)\s*{/'; 
+    $param_pattern = '/\$(\w+)/';
+
+    $modified_lines = [];
+    $inside_method = false;
+
+    foreach ($lines as $line) {
+        $modified_lines[] = $line;
+
+        if (preg_match($method_pattern, $line, $method_match)) {
+            // Found a method definition
+            $method_name = $method_match[1];
+            $params = $method_match[2];
+
+            // Add error_log for the file and line
+            $modified_lines[] = "    error_log(__FILE__.':'.__LINE__);";
+
+            // Add error_log for each parameter
+            if (preg_match_all($param_pattern, $params, $param_matches)) {
+                foreach ($param_matches[0] as $param) {
+                    $modified_lines[] = "    error_log('$param: ' . print_r($param, true));";
+                    $modified_lines[] = "error_log(__FILE__.':'.__LINE__ );";
+                }
+            }
+
+            $inside_method = true;
+        }
+    }
+}
+    function add_logging_to_methods_new($filePath) {
+        $content = file_get_contents($filePath);
+        $lines = explode("\n", $content);
+    
+        $method_pattern = '/function\s+\w+\s*\([^\)]*\)\s*\s*([\s\S]*?)\n\{/'; 
+        $param_pattern = '/\$(\w+)/';
+    
+        $modified_lines = [];
+        $inside_method = false;
+    
+        foreach ($lines as $line) {
+            $modified_lines[] = $line;
+    
+            if (preg_match($method_pattern, $line, $method_match)) {
+                // Found a method definition
+                $method_name = $method_match[1];
+                $params = $method_match[2];
+    
+                // Add error_log for the file and line
+                $modified_lines[] = "    error_log(__FILE__.':'.__LINE__);";
+    
+                // Add error_log for each parameter
+                if (preg_match_all($param_pattern, $params, $param_matches)) {
+                    foreach ($param_matches[0] as $param) {
+                        $modified_lines[] = "    error_log('$param: ' . print_r($param, true));";
+                        $modified_lines[] = "error_log(__FILE__.':'.__LINE__ );";
+                    }
+                }
+    
+                $inside_method = true;
+            }
+        }
+    
+
+    // Join modified lines into the new content
+    $new_content = implode("\n", $modified_lines);
+    file_put_contents($filePath, $new_content);
+}
 function insert_logging($filePath) {
     $content = file_get_contents($filePath);
     $lines = explode("\n", $content);
@@ -22,7 +123,7 @@ function insert_logging($filePath) {
             preg_match_all('/\$(\w+)/', $matches[2], $varMatches);
             foreach ($varMatches[1] as $var) {
                 $newContent[] = "\terror_log('$var: ' . print_r($$var, true));";
-                $newContent[] = "error_log(__FILE__.':'.__LINE__ );";
+               
             }
         }
         // Handle if, for, foreach, while statements without {
@@ -30,7 +131,7 @@ function insert_logging($filePath) {
             $insideBlock = true;
             $blockType = $matches[1];
             $newContent[] = $line;
-            $newContent[] = "error_log(__FILE__.':'.__LINE__ );";
+          
         }
         // Handle else statements with {
         elseif (preg_match('/else\s*{/', $line)) {
@@ -42,7 +143,7 @@ function insert_logging($filePath) {
             $insideBlock = true;
             $blockType = 'else';
             $newContent[] = $line;
-            $newContent[] = "error_log(__FILE__.':'.__LINE__ );";
+            
         }
         // Handle opening { for if, for, foreach, while, else without {
         elseif ($insideBlock && preg_match('/{/', $line)) {
@@ -52,7 +153,7 @@ function insert_logging($filePath) {
                 preg_match_all('/\$(\w+)/', $lines[$index-1], $varMatches);
                 foreach ($varMatches[1] as $var) {
                     $newContent[] = "\terror_log('$var: ' . print_r($$var, true));";
-                    $newContent[] = "error_log(__FILE__.':'.__LINE__ );";
+                   
                 }
             } else {
                 $newContent[] = "error_log(__FILE__.':'.__LINE__ );";
@@ -93,7 +194,11 @@ function insert_logging($filePath) {
     }
 
     file_put_contents($filePath, implode("\n", $newContent));
-	echo "Done with file path:".$filePath;
+    add_logging_to_methods($filePath);
+    add_logging_to_methods_new($filePath);
+    addErrorLogging($filePath);
+	
+    echo "Done with file path:".$filePath;
 }
 
 // Usage
